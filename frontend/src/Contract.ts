3edd94b50d16cbff85ce60b96c322d8812ffdd58
jerry <jerry65536@gmail.com>
2022-01-15 00:47:35 +0800

    create Contract.js

---
+++ frontend/src/Contract.ts
@@ -0,0 +1,21 @@
+import { Provider } from "@ethersproject/abstract-provider";
+import { BigNumberish, ethers, Wallet } from "ethers";
+import { KeyStore } from "./types";
+
+export class Contract {
+
+  provider: Provider
+  wallet: Wallet
+  keyStore: KeyStore
+
+  constructor() {
+    // TODO
+    this.provider = new ethers.providers.JsonRpcProvider('endpoint')
+    this.wallet = new ethers.Wallet('privateKey', this.provider)
+    this.keyStore = new ethers.Contract('address', 'abi', this.wallet) as KeyStore
+  }
+
+  redeem(nftId: BigNumberish) {
+    return this.keyStore.redeemKey(nftId)
+  }
+}
