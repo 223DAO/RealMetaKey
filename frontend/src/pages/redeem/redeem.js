39544c1cab65dc17696ea2b6cf7c37580a7905c5
littlered <sakuralittlered@gmail.com>
2022-01-16 02:21:27 +0800

    按钮样式调整

--- frontend/src/pages/redeem/redeem.js
+++ frontend/src/pages/redeem/redeem.js
@@ -3,6 +3,8 @@ import './redeem.css';
 import zora from '../../assets/zora.png';
 import opensea from '../../assets/opensea.png';
 
+var nftList = ['../../assets/zora.png', '../../assets/zora.png'];
+
 const zoraClicked = () => {
     window.open("https://zora.co/");
 }
@@ -27,21 +29,27 @@ function UnLoginRedeem() {
 
 function LoginRedeem() {
     return (
-        <div className="App-header">
+        <div className="Login-header">
             <p className='Title-Max-Font'>
                 Remaining keys in the store: 180
             </p>
-            <button className='Connect-Button'>
-            <img src={zora} width={36} height={36} alt="zora"  onClick={zoraClicked}/>
-                Zora
-            </button>
-            <button className='Connect-Button'>
-            <img src={opensea} width={36} height={36} alt="opensea"  onClick={openseaClicked}/>
-                Opensea
-            </button>
-            <button className='Connect-Button' onClick={zoraClicked}>
-                Redeem
-            </button>
+            <div className="Login-content">
+                <button className='Connect-Button'>
+                    <img src={zora} width={36} height={36} alt="zora" onClick={zoraClicked} />
+                    <p className='Title-Max-Font'>
+                        Zora
+                    </p>
+                </button>
+                <button className='Connect-Button'>
+                    <img src={opensea} width={36} height={36} alt="opensea" onClick={openseaClicked} />
+                    <p className='Title-Max-Font'>
+                        Opensea
+                    </p>
+                </button>
+                <button className='Connect-Button' onClick={zoraClicked}>
+                    Redeem
+                </button>
+            </div>
         </div>
     );
 }
@@ -51,7 +59,7 @@ export default Redeem;
 function Redeem() {
     const login = true;
     if (login) {
-       return <LoginRedeem />
+        return <LoginRedeem />
     } else {
         return <UnLoginRedeem />
     }
