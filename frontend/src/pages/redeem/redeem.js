f3392c019aaf92389acc799f5ea356a4738571c5
littlered <sakuralittlered@gmail.com>
2022-01-16 01:58:38 +0800

    redeem

--- frontend/src/pages/redeem/redeem.js
+++ frontend/src/pages/redeem/redeem.js
@@ -1,15 +1,17 @@
 import logo from '../../assets/logo.svg';
-import metamask from '../../assets/metamask.svg';
 import './redeem.css';
-import { MetaMask } from '../../MetaMask';
+import zora from '../../assets/zora.png';
+import opensea from '../../assets/opensea.png';
 
-const metaMaskClicked = () => {
-  let metaMask = new MetaMask();
-
-  metaMask.connect();
+const zoraClicked = () => {
+    window.open("https://zora.co/");
 }
 
-function Redeem() {
+const openseaClicked = () => {
+    window.open("https://opensea.io/");
+}
+
+function UnLoginRedeem() {
     return (
         <div className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
@@ -19,12 +21,38 @@ function Redeem() {
             <p className='Title-Max-Font'>
                 Connect MetaMask to redeem your keys!
             </p>
+        </div>
+    );
+}
+
+function LoginRedeem() {
+    return (
+        <div className="App-header">
+            <p className='Title-Max-Font'>
+                Remaining keys in the store: 180
+            </p>
             <button className='Connect-Button'>
-            <img src={metamask} width={36} height={36} alt="metamask"  onClick={metaMaskClicked}/>
-                Connect Wallet
+            <img src={zora} width={36} height={36} alt="zora"  onClick={zoraClicked}/>
+                Zora
+            </button>
+            <button className='Connect-Button'>
+            <img src={opensea} width={36} height={36} alt="opensea"  onClick={openseaClicked}/>
+                Opensea
+            </button>
+            <button className='Connect-Button' onClick={zoraClicked}>
+                Redeem
             </button>
         </div>
     );
 }
 
 export default Redeem;
+
+function Redeem() {
+    const login = true;
+    if (login) {
+       return <LoginRedeem />
+    } else {
+        return <UnLoginRedeem />
+    }
+}
