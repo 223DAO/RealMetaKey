fc5a6a73d70bcae374aa039d0c5442eff2efe4d2
jerry <jerry65536@gmail.com>
2022-01-17 01:47:12 +0800

    premium

--- frontend/src/pages/premium/premium.js
+++ frontend/src/pages/premium/premium.js
@@ -8,28 +8,30 @@ function Premium() {
   }
 
   return (
-    <div >
-      <h1>Welcome to the Premium Content</h1>
+    <div>
+      <h1 style={{ paddingTop: 40 }}>Welcome to the Premium Content</h1>
 
       <p>(Please switch to the Polygon mainnet, clear cookies and refresh page before the demo)</p>
 
-      <p className="unlock-content locked">Premium content is currently locked 🔒<br />
-        Supprt us on grant page, and you will get the unlock airdrop. <br />
-        Or you can buy the lock with 0.01 MATIC.</p>
+      <div className="premium">
+        <p className="unlock-content locked">Premium content is currently locked 🔒<br />
+          Supprt us on grant page, and you will get the unlock airdrop. <br />
+          Or you can buy the lock with 0.01 MATIC.</p>
 
-      <p className="unlock-content locked" onClick={onUnlockClick}>
-        <button className="button">
-          Unlock!
-        </button>
-      </p>
+        <p className="unlock-content locked" onClick={onUnlockClick}>
+          <button className="button">
+            Unlock!
+          </button>
+        </p>
 
-      <div className="unlock-content unlocked">
-        <p>Thanks for your support 🎉</p>
-        <li>
-          <p>Premium content 1</p>
-          <p>Premium content 2</p>
-          <p>Premium content 3</p>
-        </li>
+        <div className="unlock-content unlocked">
+          <p>Thanks for your support 🎉</p>
+          <div>
+            <p>Premium content 1</p>
+            <p>Premium content 2</p>
+            <p>Premium content 3</p>
+          </div>
+        </div>
       </div>
     </div>
   )
