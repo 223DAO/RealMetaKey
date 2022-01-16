bf095d61dee9e56334c0c763473be46c1db34581
jerry <jerry65536@gmail.com>
2022-01-16 17:32:14 +0800

    contract

--- contract/contracts/NFT.sol
+++ contract/contracts/NFT.sol
@@ -5,67 +5,18 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract NFT is ERC721Enumerable, ERC721URIStorage {
-
-    /**
-     * Structure to keep token totoal and remaining redemptions
-     * The total and remaining can be up to 42,949
-     */
-    struct NFTState {
-        uint32 total;
-        uint32 remaining;
-    }
-
-    //The maximum total supported
-    uint32 public constant maxTotal = 42_949;
-
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIdTracker;
-
+contract NFT is ERC721Enumerable {
     // Address of admin
-    mapping (address => bool) admins;
-    // Address of store contract
-    address public store;
+    mapping(address => bool) public admins;
 
-    // Mapping from token id to state
-    mapping(uint256 => NFTState) private _states;
+    mapping(uint256 => uint32) public total;
+    mapping(uint256 => uint32) public remain;
+
     // Mapping from token total and remain to token uri
-    mapping(uint32 => string) private _uris;
-
+    mapping(uint64 => string) private uris;
 
     constructor() ERC721("RealMetaKey", "RMK") {
         admins[msg.sender] = true;
-    }
-
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
-    internal
-    override(ERC721, ERC721Enumerable)
-    {
-        super._beforeTokenTransfer(from, to, tokenId);
-    }
-
-    function _burn(uint256 tokenId) 
-    internal 
-    override(ERC721, ERC721URIStorage) {
-        super._burn(tokenId);
-    }
-
-    function tokenURI(uint256 tokenId)
-    public
-    view
-    override(ERC721, ERC721URIStorage)
-    returns (string memory)
-    {
-        return super.tokenURI(tokenId);
-    }
-
-    function supportsInterface(bytes4 interfaceId)
-    public
-    view
-    override(ERC721, ERC721Enumerable)
-    returns (bool)
-    {
-        return super.supportsInterface(interfaceId);
     }
 
     /**
@@ -85,89 +36,73 @@ contract NFT is ERC721Enumerable, ERC721URIStorage {
     }
 
     /**
-     * Ensure that the token's total and remaining are valid
-     */
-    modifier onlyValidState(uint32 _total, uint32 _remaining) {
-        require(_total <= maxTotal, "The total number of redemptions exceeds max value");
-        require(
-            _remaining <= _total,
-            "Token's remaining redemptions can't be higher than total"
-        );
-        _;
-    }
-
-    /**
      * Admin can add admin
      */
-    function addAdmin(address _admin)
-    external
-    onlyAdmin
-    {
+    function addAdmin(address _admin) external onlyAdmin {
         admins[_admin] = true;
     }
 
     /**
      * Admin can remove admin
      */
-    function removeAdmin(address _admin)
-    external
-    onlyAdmin
-    {
+    function removeAdmin(address _admin) external onlyAdmin {
         admins[_admin] = false;
     }
 
     /**
-     * Admin can change store contract
+     * Admin can set the uri
      */
-    function setStore(address _store)
-    external
-    onlyAdmin
-    {
-        store = _store;
-    }
-
-    /**
-     * Admin can change the uri mapping
-     */
-    function setUriMapping(uint32 _id, string calldata _uri)
-    external
-    onlyAdmin
-    {
-        _uris[_id] = _uri;
+    function setUri(
+        uint32 total_,
+        uint32 remain_,
+        string calldata uri
+    ) public onlyAdmin {
+        uris[_state(total_, remain_)] = uri;
     }
 
     /**
      * Only store contract can mint
      * Mint token to _to, with _total total and _remain remain
      */
-    function mint(address _to, uint32 _total, uint32 _remaining)
-    external
-    onlyAdmin
-    {
-        uint32 id = uriMappingID(_total, _remaining);
-        string memory _uri = _uris[id];
-        require(_uri != "", "No content representing the total and remaining");
-
-        uint256 _tokenId = _tokenIdTracker.current();
-        ERC721._safeMint(_to, _tokenId);
-        _tokenIdTracker.increment();
-
-        ERC721URIStorage._setTokenURI(_tokenId, _uri);
-        _states[_tokenId] = NFTState(
-            {
-                total: _total,
-                remaining: _remaining
-            }
+    function mint(
+        address _to,
+        uint32 _total,
+        uint32 _remaining
+    ) external onlyAdmin {
+        require(_total > 0, "total must be greater to 0");
+        require(
+            _remaining > 0 && _remaining <= _total,
+            "remain must be greater than 0 and less equal total"
         );
+
+        uint256 _tokenId = totalSupply();
+        super._safeMint(_to, _tokenId);
+
+        total[_tokenId] = _total;
+        remain[_tokenId] = _remaining;
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        return uris[_tokenState(tokenId)];
+    }
+
+    function setRemain(uint256 _tokenId, uint32 _remaining) public onlyAdmin {
+        remain[_tokenId] = _remaining;
     }
 
     /**
      * Get all token ids of _owner
      */
     function getTokenIds(address _owner)
-    external
-    view
-    returns (uint256[] memory)
+        external
+        view
+        returns (uint256[] memory)
     {
         uint256 _tokenCount = ERC721.balanceOf(_owner);
         if (_tokenCount == 0) {
@@ -177,78 +112,21 @@ contract NFT is ERC721Enumerable, ERC721URIStorage {
         uint256[] memory _tokenIDs = new uint256[](_tokenCount);
         uint256 i;
 
-        for (i=0;i<_tokenCount;i++){
+        for (i = 0; i < _tokenCount; i++) {
             _tokenIDs[i] = ERC721Enumerable.tokenOfOwnerByIndex(_owner, i);
         }
         return _tokenIDs;
     }
 
-    /**
-     * Get token total redemption count from _tokenId
-     */
-    function getTotal(uint256 _tokenId)
-    external
-    view
-    onlyExistingToken(_tokenId)
-    returns (uint32)
-    {
-        return _states[_tokenId].total;
+    function _tokenState(uint256 tokenId) private view returns (uint64) {
+        return (uint32(total[tokenId]) << 16) | remain[tokenId];
     }
 
-    /**
-     * Get token remaining redemption count from _tokenId
-     */
-    function getRemaining(uint256 _tokenId)
-    external
-    view
-    onlyExistingToken(_tokenId)
-    returns (uint32)
+    function _state(uint32 total_, uint32 remain_)
+        private
+        pure
+        returns (uint64)
     {
-        return _states[_tokenId].remaining;
-    }
-
-    /**
-     * Get token URI from _tokenId
-     */
-    function getTokenUri(uint256 _tokenId)
-    external
-    view
-    onlyExistingToken(_tokenId)
-    returns (string memory)
-    {
-        return ERC721URIStorage.tokenURI(_tokenId);
-    }
-
-    /**
-     * Update remaining total and remaining redemption count and uri for token with _tokenId
-     */
-    function redeem(uint256 _tokenId, uint32 _total, uint32 _remaining)
-    external
-    onlyAdmin
-    onlyExistingToken(_tokenId)
-    {
-        uint32 id = uriMappingID(_total, _remaining);
-        string memory _uri = _uris[id];
-        require(_uri != "", "No content representing the total and remaining");
-
-        ERC721URIStorage._setTokenURI(_tokenId, _uri);
-        _states[_tokenId] = NFTState(
-            {
-                total: _total,
-                remaining: _remaining
-            }
-        );
-    }
-
-    /**
-     * Get URI mapping id from total and remaining
-     */
-    function uriMappingID(uint32 _total, uint32 _remaining)
-    private
-    pure
-    onlyValidState(_total, _remaining)
-    returns (uint32)
-    {
-        return 100_000*_total + _remaining;
+        return (uint64(total_) << 16) | uint64(remain_);
     }
 }
