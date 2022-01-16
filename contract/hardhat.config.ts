8fce849c493a3e3eb73057ad92b82051432fcf5a
gyczzm <gyczzm@gmail.com>
2022-01-16 19:08:39 +0800

    Added deploy script

--- contract/hardhat.config.ts
+++ contract/hardhat.config.ts
@@ -1,51 +1,65 @@
-import * as dotenv from "dotenv";
-
-import { HardhatUserConfig, task } from "hardhat/config";
-import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
-import "@typechain/hardhat";
-import "hardhat-gas-reporter";
-import "solidity-coverage";
+import '@typechain/hardhat'
+import 'hardhat-deploy';
+import { HardhatUserConfig } from "hardhat/config";
 
-dotenv.config();
+// admin account for test deploy on goerli
+let adminPrivateKeyTest = ''
+try {
+  adminPrivateKeyTest = require('./secrets/admin-account-test.json')[0].privateKey
+} catch (e) {
+  console.error(e)
+}
 
-const RPC_GOERLI = 'https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161' // from MetaMask
-const RPC_MATIC = 'https://rpc-mainnet.maticvigil.com'
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+const rpcMumbai = 'https://matic-mumbai.chainstacklabs.com' // from MetaMask
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.4",
+  // Your type-safe config goes here
+  solidity: {
+    version: "0.8.0"
+  },
+  defaultNetwork: 'hardhat',
   networks: {
-    ropsten: {
-      url: RPC_GOERLI,
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    // for test
+    mumbai: {
+      url: rpcMumbai,
+      accounts: [
+        adminPrivateKeyTest
+      ]
     },
-    matic: {
-      url: RPC_MATIC,
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
-    }
+    hardhat: {},
   },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
+  typechain: {
+    outDir: 'plugins/types',
+    // target: 'ethers-v5',
+    alwaysGenerateOverloads: false, // should overloads with full signatures like deposit(uint256) be generated always, even if there are no overloads?
+    // externalArtifacts: ['externalArtifacts/*.json'], // optional array of glob patterns with external artifacts to process (for example external libs from node_modules)
   },
-  // etherscan: {
-  //   apiKey: process.env.ETHERSCAN_API_KEY,
-  // },
+  namedAccounts: {
+    deployer: 0,
+  }
 };
 
 export default config;
+
+
+// Network stuff for later
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+// export default {
+//   solidity: "0.8.4",
+//   // networks: {
+//   //   hardhat: {
+//   //     // accounts: {
+//   //     //   accountsBalance: "10",
+//   //     // },
+//   //     forking: {
+//   //       enabled: true,
+//   //       url: "https://xdai-archive.blockscout.com/",
+//   //       blockNumber: 17615631, //17615631
+//   //     },
+//   //   },
+//   // },
+// };
--- contract/scripts/deploy.ts
+++ contract/scripts/deploy.ts
@@ -1,30 +1,17 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
-import { ethers } from "hardhat";
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+import { DeployFunction } from 'hardhat-deploy/types';
 
-async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy } = deployments;
 
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const { deployer } = await getNamedAccounts();
 
-  await greeter.deployed();
+  await deploy('NFT', {
+    from: deployer,
+    log: true,
+  });
+};
 
-  console.log("Greeter deployed to:", greeter.address);
-}
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+export default func;
+func.tags = ['NFT'];
